4f01efa0496d00120842f963137137a44a480fd3
Roman Shapiro <roman.shapiro@grapecity.com>
2020-05-22 22:06:46 +0700

    Fixes #15

--- src/TroublesOfJord.Game/UI/GameView.cs
+++ src/TroublesOfJord.Game/UI/GameView.cs
@@ -13,33 +13,12 @@ namespace TroublesOfJord.UI
 		public MapView MapView { get; } = new MapView();
 		public MiniMap MapNavigation { get; } = new MiniMap();
 		public LogView LogView { get; } = new LogView();
-		public readonly SkillWidget[] Skills = new SkillWidget[10];
 
 		protected override bool AcceptsKeyboardFocus => true;
 
 		public GameView()
 		{
 			BuildUI();
-
-			_skillsContainer.Widgets.Add(new VerticalSeparator());
-			for (var i = 0; i < Skills.Length; ++i)
-			{
-				var skillWidget = new SkillWidget
-				{
-					Width = 50
-				};
-				skillWidget._labelIndex.Text = i < Skills.Length - 1 ? (i + 1).ToString() : 0.ToString();
-
-				skillWidget._checkBoxAuto.Visible = false;
-				skillWidget._buttonSkill.Visible = false;
-
-				skillWidget._buttonSkill.TouchDown += _buttonSkill_TouchDown;
-
-				Skills[i] = skillWidget;
-				
-				_skillsContainer.Widgets.Add(Skills[i]);
-				_skillsContainer.Widgets.Add(new VerticalSeparator());
-			}
 
 			_mapViewContainer.Widgets.Add(MapView);
 
@@ -48,8 +27,6 @@ namespace TroublesOfJord.UI
 
 			LogView.ShowVerticalScrollBar = false;
 			_logContainer.Widgets.Add(LogView);
-
-			UpdateSkills();
 		}
 
 		private void _buttonSkill_TouchDown(object sender, EventArgs e)
@@ -58,29 +35,6 @@ namespace TroublesOfJord.UI
 			var ability = (IUsableAbility)widget.Tag;
 
 			ability.Use();
-		}
-
-		private void UpdateSkills()
-		{
-			var usableAbilities = TJ.Player.UsableAbilities;
-
-			var i = 0;
-			for (; i < Math.Min(usableAbilities.Length, Skills.Length); ++i)
-			{
-				var ability = usableAbilities[i];
-				var skillWidget = Skills[i];
-				skillWidget._checkBoxAuto.Visible = ability.CanAuto;
-				skillWidget._buttonSkill.Text = ability.Name;
-				skillWidget._buttonSkill.Visible = true;
-				skillWidget.Tag = ability;
-			}
-
-			for (; i < Skills.Length; ++i)
-			{
-				var skillWidget = Skills[i];
-				skillWidget._checkBoxAuto.Visible = false;
-				skillWidget._buttonSkill.Visible = false;
-			}
 		}
 
 		private bool ProcessMovement(ref KeyboardState keys)
